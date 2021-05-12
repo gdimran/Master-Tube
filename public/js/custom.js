b0be65515b3c5b8366ce0a79c4c9fe1441c91da6
gdimran <imran_hossain.gd98@yahoo.com>
2021-05-12 20:47:53 +0600

    home-page,user-accesspage done

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,63 +1,13 @@
 
-$('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
 
-  $(document).ready(function() {
-    $(".myvideos").on("mouseover", function(event) {
-      this.play();
-  
-    }).on('mouseout', function(event) {
-      this.pause();
-  
-    });
-  })
-  var cip = $(".video").hover( hoverVideo, hideVideo );
+ //video open-close 
+ var videoPlayer=document.getElementById("videoPlayer");
+ var singleVideo=document.getElementById("singleVideo");
 
-function hoverVideo(e) {  
-    $('video', this).get(0).play(); 
-}
-
-function hideVideo(e) {
-    $('video', this).get(0).pause(); 
-}
+ function stopVideo(){
+    document.getElementById("videoPlayer").style.display="none";
+ }
+ function playVideo(file){
+    document.getElementById("singleVideo").src=file;
+    document.getElementById("videoPlayer").style.display="block";
+ }
