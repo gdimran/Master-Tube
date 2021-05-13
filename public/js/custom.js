f54fa8f2c71af1d0447b1127e98ffae24411a372
gdimran <imran_hossain.gd98@yahoo.com>
2021-05-13 18:12:40 +0600

    single-content page, popups, book page user-logedin headers are done

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,3 +11,30 @@
     document.getElementById("singleVideo").src=file;
     document.getElementById("videoPlayer").style.display="block";
  }
+
+ //=============Tab content=================
+ function openDesc(evt, tabName) {
+   var i, tabcontent, tablinks;
+   tabcontent = document.getElementsByClassName("tabcontent");
+   for (i = 0; i < tabcontent.length; i++) {
+     tabcontent[i].style.display = "none";
+   }
+   tablinks = document.getElementsByClassName("tablinks");
+   for (i = 0; i < tablinks.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" active", "");
+   }
+   document.getElementById(tabName).style.display = "block";
+   evt.currentTarget.className += " active";
+ }
+
+
+ //================popups=====================
+ function createOption() {
+  var popup = document.getElementById("createOptionPopup");
+  popup.classList.toggle("show");
+}
+
+function notificationFunc() {
+  var popup = document.getElementById("notificationPopup");
+  popup.classList.toggle("show");
+}
