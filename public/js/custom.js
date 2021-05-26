c8cbc25bf50922b9477eb15a8bf407f1efd9b12b
gdimran <imran_hossain.gd98@yahoo.com>
2021-05-26 18:47:10 +0600

    All fonts and other issues fixed as youtube. Dark mode also done

--- public/js/custom.js
+++ public/js/custom.js
@@ -19,60 +19,39 @@ function videoOption() {
   var popup = document.getElementById("videoOptionPopup");
   popup.classList.toggle("show");
 }
+
+function addQueueOption() {
+  var popup = document.getElementById("addToQue");
+  popup.classList.toggle("show");
+}
+
 //==================overlay side bar===========================
 function openOverlaySidebar() {
   document.getElementById("overSidepanel").style.display = "block";
   document.getElementById("pageOverlay").style.display = "block";
+  document.getElementById("content").style.width = "calc(100% - 75px)";
 }
 
 function closeOverlaySidebar() {
   document.getElementById("overSidepanel").style.display = "none";
   document.getElementById("pageOverlay").style.display = "none";
+  document.getElementById("content").style.width = "calc(100% - 75px)";
 }
 
 //=============expandable side bar=============================
 
-
-// window.onload = function () {
-
-//   if (document.getElementById("exSidepanel") === "none") {
-//     document.getElementById("content").style.width = "calc(100% - 75px)";
-//   } else {
-//     document.getElementById("content").style.width = "calc(100% - 260px)";
-//     document.getElementById("content").style.marginLeft = "185px";
-//   }
-// }
-
 window.onload = function setContentWidth() {
-
-  if (document.getElementById("exSidepanel").style.display === "block") {
+  var element = document.getElementById('exSidepanel');
+  if (typeof (element) != 'undefined' && element != null) {
     document.getElementById("side-bar").style.display = "none";
     document.getElementById("content").style.width = "calc(100% - 260px)";
     document.getElementById("content").style.marginLeft = "185px";
-  } else if (document.getElementById("exSidepanel").style.display === "none" && document.getElementById("side-bar").style.display === "block") {
-    document.getElementById("exSidepanel").style.display = "none";
-    document.getElementById("content").style.width = "calc(100% - 75px)";
   } else {
-    document.getElementById("exSidepanel").style.display = "none";
-    document.getElementById("side-bar").style.display = "none";
     document.getElementById("content").style.width = "calc(100% - 75px)";
   }
 }
 
 
-
-// window.onload = function () {
-
-//   if (document.getElementById("exSidepanel") === "none") {
-//     document.getElementById("content").style.width = "calc(100% - 260px)";
-//     document.getElementById("content").style.marginLeft = "185px";
-//   }
-//   else {
-//     document.getElementById("content").style.width = "calc(100% - 75px)";
-//   }
-
-
-// }
 
 function showExpandsideBar() {
   var x = document.getElementById("exSidepanel");
@@ -128,3 +107,63 @@ commentInput.onkeyup = function () {
 function closeComment() {
   commentSubmit.style.display = "none";
 }
+
+function replyComment() {
+  document.getElementById("replay-input").style.display = "block";
+}
+
+
+//============set id using js============
+
+// var singleElements = document.querySelectorAll('.single-video-content');
+
+// // Set their ids
+// for (var i = 0; i < singleElements.length; i++) {
+//   singleElements[i].id = 'singleVideo-' + i;
+// }
+
+//==================dark mode theme===========================
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('darkmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('darkmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode');
+
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    document.getElementById("logo-dark").style.display = "block";
+    document.getElementById("logo-light").style.display = "none";
+    // if it has been enabled, turn it off  
+  } else {
+    disableDarkMode();
+    document.getElementById("logo-dark").style.display = "none";
+    document.getElementById("logo-light").style.display = "block";
+  }
+});
+
+
